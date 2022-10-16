3fb8b2265861ca841e709995f2eb19b163fef13a
Colexeco <evan.shoemaker0@gmail.com>
2022-10-15 17:15:32 -0700

    moved navigation icons to src

--- react_insta_clone/src/App.js
+++ react_insta_clone/src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="container">
       <nav>
         <p>Instagram Clone</p>
-        <img src="../public/home-outline.png" alt="home"></img>
-        <img src="../public/message-circle-outline.png" alt="search"></img>
-        <img src="../public/search-outline.png" alt="messages"></img>
+        <img src="./home-outline.png" alt="home"></img>
+        <img src="./message-circle-outline.png" alt="search"></img>
+        <img src="./search-outline.png" alt="messages"></img>
       </nav>
     </div>
   );
