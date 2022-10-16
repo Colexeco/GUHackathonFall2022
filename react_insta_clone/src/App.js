28817a427e176255abad47ee64dc28761326e354
Colexeco <evan.shoemaker0@gmail.com>
2022-10-15 17:32:16 -0700

    added temporary redirect doc

--- react_insta_clone/src/App.js
+++ react_insta_clone/src/App.js
@@ -6,10 +6,15 @@ function App() {
     <div className="container">
       <nav>
         <p>Instagram Clone</p>
-        <img src="./home-outline.png" alt="home"></img>
-        <img src="./message-circle-outline.png" alt="search"></img>
-        <img src="./search-outline.png" alt="messages"></img>
+        <p><img src="./home-outline.png" alt="home"></img></p>
+        <p><img src="./message-circle-outline.png" alt="search"></img></p>
+        <p><img src="./search-outline.png" alt="messages"></img></p>
       </nav>
+      <div className="form">
+        <form>
+          
+        </form>
+      </div>
     </div>
   );
 }
